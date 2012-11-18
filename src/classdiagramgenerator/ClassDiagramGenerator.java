730ea867c11b4ba7876f71f0f4a1a423b9ae3caf
Jake Rego <regojr@muohio.edu>
2012-11-18 17:12:23 -0500

    First commit. GUI included

---
+++ src/classdiagramgenerator/ClassDiagramGenerator.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classdiagramgenerator;
+
+/**
+ *
+ * @author Jake
+ */
+public class ClassDiagramGenerator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new HomePage().setVisible(true);        
+    }
+}
